9eb827cf5c499d366d9b1750a1bff524b6a68a4d
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-11 10:45:29 -0600

    Added More Java Code

--- Develop/script.js
+++ Develop/script.js
@@ -2,25 +2,50 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-//Ignore the top part ^^^^^^^^ Its done
-document.addEventListener("DOMContentLoaded", function() {
-  const saveButtons = document.querySelectorAll(".saveBtn");
+// Gets the current date and time
+const currentDate = new Date();
 
-saveButtons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    const textArea = this.previousElementSibling;
-    const textAreaValue = textArea.value.trim();
-    const timeBlockId = textArea.parentElement.id;
-    localStorage.setItem(timeBlockId, textAreaValue);
+// Gets the current hour in 24-hour format
+let currentHour24 = currentDate.getHours();
+
+// Converts the current hour to 12-hour format
+let currentHour12 = currentHour24 % 12;
+if (currentHour12 === 0) {
+  currentHour12 = 12;
+}
+
+// Adds the "past", "present", or "future" class to each time block
+const timeBlocks = document.querySelectorAll(".time-block");
+
+timeBlocks.forEach(function (timeBlock) {
+  const timeBlockHour = parseInt(timeBlock.id.split("-")[1]);
+
+  if (timeBlockHour < currentHour24) {
+    timeBlock.classList.add("past");
+  } else if (timeBlockHour === currentHour24) {
+    timeBlock.classList.add("present");
+  } else {
+    timeBlock.classList.add("future");
+  }
+  
+  // Converts the time block hour to 12-hour format
+  let timeBlockHour12 = timeBlockHour % 12;
+  if (timeBlockHour12 === 0) {
+    timeBlockHour12 = 12;
+  }
+
+  // Displays the time in 12-hour format
+  const timeBlockDisplay = timeBlock.querySelector(".hour");
+  timeBlockDisplay.textContent = `${timeBlockHour12} ${timeBlockHour12 >= 12 ? "PM" : "AM"}`;
 });
 
-});
 
-});
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage. Finished?
+  
+  // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
@@ -29,10 +54,11 @@ saveButtons.forEach(function(button) {
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // current hour in 24-hour time? Finished?j
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // attribute of each time-block be used to do this? Finished?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page. Finished
+Finished
